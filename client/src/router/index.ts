56e5decee5d243b339c282bb8eb96f2718106f87
Carolyn <31444802+carolaguivel@users.noreply.github.com>
2026-04-15 17:08:08 +0200

    fix dependencies

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
-import { ROUTE_NAMES, ROUTES } from "@/app.constants";
 import HomeView from "@/views/HomeView.vue";
+import { ROUTE_NAMES, ROUTES } from "@/app.constants";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
