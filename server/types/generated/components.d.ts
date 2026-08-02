e6d93e880b0eb57c63c0620953bf9a32ac55c5e7
carolaguivel <>
2026-08-02 14:20:58 +0200

    updates

--- server/types/generated/components.d.ts
+++ server/types/generated/components.d.ts
@@ -95,7 +95,7 @@ export interface SharedNewsletter extends Struct.ComponentSchema {
 }
 
 declare module '@strapi/strapi' {
-  export module Public {
+  export namespace Public {
     export interface ComponentSchemas {
       'blocks.hero': BlocksHero;
       'layout.footer': LayoutFooter;
